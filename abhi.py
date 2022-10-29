98c6221b8e8427a6191e7aff4f3270fbed493e4c
Abhishek <patelabhishekaa5@gmail.com>
2022-10-29 13:39:09 +0530

    8th commite add pythode code

---
+++ abhi.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from subprocess import Popen,PIPE
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "Root_hit"
+
+@app.route("/app1")
+def app1():
+    process = Popen(['hostname'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    stdout=str(stdout.decode())
+    return f"app1_hit from ----------->>> {stdout}"
+
+@app.route("/app2")
+def app2():
+    process = Popen(['hostname'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    stdout=str(stdout.decode())
+    return f"app2_hit from ----------->>> {stdout}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+app.run(debug=True)
